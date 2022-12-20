a109a23f080d1d54efe0a90313575d710de37cec
Amit Dahan <amitdahan11@gmail.com>
2022-12-20 19:29:29 +0200

    Add Day 20 @ 2022

---
+++ src/year_2022/day_20/mod.rs
@@ -0,0 +1,119 @@
+use nom::{
+    character::complete::{i64, newline},
+    multi::separated_list1,
+    IResult,
+};
+use std::cmp::Ordering::{Equal, Greater, Less};
+use std::collections::VecDeque;
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("example.in");
+    const INPUT: &str = include_str!("input.in");
+
+    mod part1 {
+        use super::*;
+
+        #[test]
+        fn it_solves_example() {
+            let result = solve_part1(EXAMPLE);
+            assert_eq!(result, 3);
+        }
+
+        #[test]
+        fn it_solves_input() {
+            let result = solve_part1(INPUT);
+            assert_eq!(result, 11_037);
+        }
+    }
+
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn it_solves_example() {
+            let result = solve_part2(EXAMPLE);
+            assert_eq!(result, 1_623_178_306);
+        }
+
+        #[test]
+        fn it_solves_input() {
+            let result = solve_part2(INPUT);
+            assert_eq!(result, 3_033_720_253_914);
+        }
+    }
+}
+
+fn line(input: &str) -> IResult<&str, i64> {
+    i64(input)
+}
+fn parse(input: &str) -> Vec<i64> {
+    separated_list1(newline, line)(input).unwrap().1
+}
+
+fn mix(numbers: &[i64], indices: &mut VecDeque<usize>) {
+    for (i, &n) in numbers.iter().enumerate() {
+        let i = indices.iter().position(|&x| x == i).unwrap();
+        match n.cmp(&0) {
+            Less => {
+                let value = indices.remove(i).unwrap();
+                let rotate_by = n.unsigned_abs() as usize % indices.len();
+                indices.rotate_right(rotate_by);
+                indices.insert(i, value);
+            }
+            Greater => {
+                let value = indices.remove(i).unwrap();
+                let rotate_by = n.unsigned_abs() as usize % indices.len();
+                indices.rotate_left(rotate_by);
+                indices.insert(i, value);
+            }
+            Equal => {}
+        }
+    }
+}
+
+pub fn solve_part1(input: &str) -> i64 {
+    let numbers = parse(input);
+
+    let mut original_indices: VecDeque<usize> =
+        numbers.iter().enumerate().map(|(i, _)| i).collect();
+    mix(&numbers, &mut original_indices);
+
+    let zero_idx = numbers.iter().position(|&x| x == 0).unwrap();
+    let zero_idx = original_indices
+        .iter()
+        .position(|&x| x == zero_idx)
+        .unwrap();
+
+    vec![1_000, 2_000, 3_000]
+        .iter()
+        .map(|i| numbers[original_indices[(zero_idx + i) % numbers.len()]])
+        .sum()
+}
+
+pub fn solve_part2(input: &str) -> i64 {
+    const KEY: i64 = 811_589_153;
+
+    let numbers = parse(input);
+    let numbers = numbers.iter().map(|&n| n * KEY).collect::<Vec<_>>();
+
+    let mut original_indices: VecDeque<usize> =
+        numbers.iter().enumerate().map(|(i, _)| i).collect();
+
+    for _ in 0..10 {
+        mix(&numbers, &mut original_indices);
+    }
+
+    let zero_idx = numbers.iter().position(|&x| x == 0).unwrap();
+    let zero_idx = original_indices
+        .iter()
+        .position(|&x| x == zero_idx)
+        .unwrap();
+
+    vec![1000, 2000, 3000]
+        .iter()
+        .map(|i| numbers[original_indices[(zero_idx + i) % numbers.len()]])
+        .sum()
+}
--- src/year_2022/mod.rs
+++ src/year_2022/mod.rs
@@ -16,3 +16,4 @@ pub mod day_14;
 pub mod day_15;
 pub mod day_16;
 pub mod day_18;
+pub mod day_20;
