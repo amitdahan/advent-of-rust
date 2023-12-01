7beb05604c398bb8c43025f833f29bca4b4932cb
Amit Dahan <amitdahan11@gmail.com>
2023-12-01 11:45:16 +0200

    Add Day 1 @ 2023

--- src/year_2023/day_1/mod.rs
+++ src/year_2023/day_1/mod.rs
@@ -2,7 +2,8 @@
 mod tests {
     use super::*;
 
-    const EXAMPLE: &str = include_str!("example.in");
+    const EXAMPLE_1: &str = include_str!("example_1.in");
+    const EXAMPLE_2: &str = include_str!("example_2.in");
     const INPUT: &str = include_str!("input.in");
 
     mod part1 {
@@ -10,14 +11,14 @@ mod tests {
 
         #[test]
         fn it_solves_example() {
-            let result = solve_part1(EXAMPLE);
-            assert_eq!(result, 24000);
+            let result = solve_part1(EXAMPLE_1);
+            assert_eq!(result, 142);
         }
 
         #[test]
         fn it_solves_input() {
             let result = solve_part1(INPUT);
-            assert_eq!(result, 70698);
+            assert_eq!(result, 54968);
         }
     }
 
@@ -26,44 +27,92 @@ mod tests {
 
         #[test]
         fn it_solves_example() {
-            let result = solve_part2(EXAMPLE);
-            assert_eq!(result, 45000);
+            let result = solve_part2(EXAMPLE_2);
+            assert_eq!(result, 281);
         }
 
         #[test]
         fn it_solves_input() {
             let result = solve_part2(INPUT);
-            assert_eq!(result, 206643);
+            assert_eq!(result, 54094);
         }
     }
 }
 
-pub fn solve_part1(input: &str) -> i32 {
-    input
-        .split("\n\n")
-        .map(|group| {
-            group
-                .lines()
-                .map(|line| line.parse::<i32>().unwrap())
-                .sum::<i32>()
-        })
-        .max()
-        .unwrap()
+pub fn solve_part1(input: &str) -> u32 {
+    let mut sum = 0;
+
+    for row in input.lines() {
+        let first_digit = row
+            .chars()
+            .find(|c| c.is_numeric())
+            .map(|c| c.to_digit(10))
+            .unwrap()
+            .unwrap();
+        let last_digit = row
+            .chars()
+            .rev()
+            .find(|c| c.is_numeric())
+            .map(|c| c.to_digit(10))
+            .unwrap()
+            .unwrap();
+
+        sum += first_digit * 10 + last_digit;
+    }
+
+    sum
 }
 
-pub fn solve_part2(input: &str) -> i32 {
-    let mut calories = input
-        .split("\n\n")
-        .map(|group| {
-            group
-                .lines()
-                .map(|line| line.parse::<i32>().unwrap())
-                .sum::<i32>()
-        })
-        .collect::<Vec<i32>>();
+const DIGIT_WORDS: [&str; 9] = [
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+];
 
-    calories.sort_by(|a, b| a.cmp(b).reverse());
-    calories.truncate(3);
+pub fn solve_part2(input: &str) -> u32 {
+    let mut sum = 0;
 
-    calories.iter().sum()
+    for row in input.lines() {
+        let first_digit = row
+            .char_indices()
+            .find_map(|(i, c)| {
+                if c.is_numeric() {
+                    Some(c.to_digit(10).unwrap())
+                } else {
+                    DIGIT_WORDS
+                        .iter()
+                        .enumerate()
+                        .find_map(|(digit_i, digit_str)| {
+                            if row.get(i..).unwrap().starts_with(digit_str) {
+                                Some(digit_i as u32 + 1)
+                            } else {
+                                None
+                            }
+                        })
+                }
+            })
+            .unwrap();
+
+        let last_digit = row
+            .char_indices()
+            .rev()
+            .find_map(|(i, c)| {
+                if c.is_numeric() {
+                    Some(c.to_digit(10).unwrap())
+                } else {
+                    DIGIT_WORDS
+                        .iter()
+                        .enumerate()
+                        .find_map(|(digit_i, digit_str)| {
+                            if row.get(..=i).unwrap().ends_with(digit_str) {
+                                Some(digit_i as u32 + 1)
+                            } else {
+                                None
+                            }
+                        })
+                }
+            })
+            .unwrap();
+        sum += first_digit * 10 + last_digit;
+    }
+
+    sum
 }
