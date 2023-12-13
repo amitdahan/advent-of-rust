deddb726f2d6927d19a55831f2d132724dd61674
Amit Dahan <amitdahan11@gmail.com>
2023-12-13 11:35:36 +0200

    Add Day 12 @ 2023

---
+++ src/year_2023/day_12/mod.rs
@@ -0,0 +1,164 @@
+use std::collections::HashMap;
+
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{newline, u32},
+    combinator::map,
+    multi::{many1, separated_list1},
+    sequence::separated_pair,
+    IResult,
+};
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const EXAMPLE: &str = include_str!("example.in");
+
+    mod part1 {
+        use super::*;
+
+        #[test]
+        fn it_solves_example() {
+            let result = solve_part1(EXAMPLE);
+            assert_eq!(result, 21);
+        }
+    }
+
+    mod part2 {
+        use super::*;
+
+        #[test]
+        fn it_solves_example() {
+            let result = solve_part2(EXAMPLE);
+            assert_eq!(result, 525152);
+        }
+    }
+}
+
+#[derive(Debug, Clone, PartialEq)]
+enum Status {
+    Operational,
+    Damaged,
+    Unknown,
+}
+
+fn parse_status(input: &str) -> IResult<&str, Status> {
+    alt((
+        map(tag("."), |_| Status::Operational),
+        map(tag("#"), |_| Status::Damaged),
+        map(tag("?"), |_| Status::Unknown),
+    ))(input)
+}
+fn parse(input: &str) -> IResult<&str, Vec<(Vec<Status>, Vec<u32>)>> {
+    separated_list1(
+        newline,
+        separated_pair(
+            many1(parse_status),
+            tag(" "),
+            separated_list1(tag(","), u32),
+        ),
+    )(input)
+}
+
+type CacheKey = (usize, usize, Option<u32>);
+
+fn count_arrangements(
+    statuses: &[Status],
+    groups: &[u32],
+    consecutive: Option<u32>,
+    cache: &mut HashMap<CacheKey, u64>,
+) -> u64 {
+    if let Some(curr) = statuses.first() {
+        use Status::*;
+
+        let key = (statuses.len(), groups.len(), consecutive);
+
+        if let Some(value) = cache.get(&key) {
+            return *value;
+        }
+
+        let value = match (curr, &consecutive, groups.first()) {
+            (_, Some(_), None) => 0,
+            (Operational, Some(consecutive), Some(group)) if consecutive != group => 0,
+            (Operational, None, _) => count_arrangements(&statuses[1..], groups, None, cache),
+            (Damaged, _, None) => 0,
+            (Damaged, Some(consecutive), Some(group)) if consecutive >= group => 0,
+            (Damaged, Some(consecutive), Some(_)) => {
+                count_arrangements(&statuses[1..], groups, Some(consecutive + 1), cache)
+            }
+            (Damaged, None, _) => count_arrangements(&statuses[1..], groups, Some(1), cache),
+            (Unknown, None, None) => count_arrangements(&statuses[1..], groups, None, cache),
+            (Unknown, None, Some(_)) => {
+                count_arrangements(&statuses[1..], groups, None, cache)
+                    + count_arrangements(&statuses[1..], groups, Some(1), cache)
+            }
+            (Unknown, Some(consecutive), Some(group)) if consecutive > group => 0,
+            (Unknown, Some(consecutive), Some(group)) if consecutive < group => {
+                count_arrangements(&statuses[1..], groups, Some(consecutive + 1), cache)
+            }
+            (_, Some(_), Some(_)) => count_arrangements(&statuses[1..], &groups[1..], None, cache),
+        };
+        cache.insert(key, value);
+        value
+    } else {
+        match (consecutive, groups.len(), groups.first()) {
+            (None, 0, _) => 1,
+            (None, _, None) => 1,
+            (Some(consecutive), 1, Some(group)) if consecutive == *group => 1,
+            _ => 0,
+        }
+    }
+}
+
+fn unfold(statuses: &[Status], groups: &[u32]) -> (Vec<Status>, Vec<u32>) {
+    let statuses = statuses.iter().cloned().collect::<Vec<_>>();
+    let groups = groups.iter().cloned().collect::<Vec<_>>();
+
+    let statuses = [
+        statuses.clone(),
+        vec![Status::Unknown],
+        statuses.clone(),
+        vec![Status::Unknown],
+        statuses.clone(),
+        vec![Status::Unknown],
+        statuses.clone(),
+        vec![Status::Unknown],
+        statuses,
+    ]
+    .into_iter()
+    .flatten()
+    .collect::<Vec<_>>();
+
+    let groups = [
+        groups.clone(),
+        groups.clone(),
+        groups.clone(),
+        groups.clone(),
+        groups,
+    ]
+    .into_iter()
+    .flatten()
+    .collect::<Vec<_>>();
+
+    (statuses, groups)
+}
+fn solve(rows: &[(Vec<Status>, Vec<u32>)]) -> u64 {
+    rows.iter()
+        .map(|(statuses, groups)| count_arrangements(statuses, groups, None, &mut HashMap::new()))
+        .sum()
+}
+pub fn solve_part1(input: &str) -> u64 {
+    let (_, rows) = parse(input).unwrap();
+    solve(&rows)
+}
+pub fn solve_part2(input: &str) -> u64 {
+    let (_, rows) = parse(input).unwrap();
+    let rows = rows
+        .iter()
+        .map(|(statuses, groups)| unfold(statuses, groups))
+        .collect::<Vec<_>>();
+
+    solve(&rows)
+}
--- src/year_2023/mod.rs
+++ src/year_2023/mod.rs
@@ -1,6 +1,7 @@
 pub mod day_1;
 pub mod day_10;
 pub mod day_11;
+pub mod day_12;
 pub mod day_2;
 pub mod day_3;
 pub mod day_4;
